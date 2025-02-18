e9812e97f5c25487964b7e803921cafd31d3ea01
MortiestMorty <d.arroyo14@student.keiseruniversity.edu>
2025-02-17 23:12:54 -0500

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,16 @@
+/*
+Consider the following recursive function:
+Click here to view code image
+public static int mystery(int a, int b)
+{
+if (b == 0) return 0;
+if (b % 2 == 0) return mystery(a+a, b/2);
+return mystery(a+a, b/2) + a;
+}
+What are the values of mystery(2, 25) and mystery(3, 11)? Given positive integers a and b,
+describe what value mystery(a, b) computes. Answer the same question, but replace + with *
+and replace return 0 with return 1.
+*/
 public class Main
 {
         public static int mystery(int a, int b)
@@ -5,10 +18,10 @@ public class Main
             if (b == 0) return 0;
             if (b % 2 == 0) return mystery(a+a, b/2);
             return mystery(a+a, b/2) + a;
-        }
+        }//end mystery method
     public static void main(String[] args)
     {
         int result = mystery(3, 5);
         System.out.println(result);
-    }
-}
+    }//end main
+}//end class
